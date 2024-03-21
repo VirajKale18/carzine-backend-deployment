2868df1c2151a563b25b026cd9e01bfba639f981
VirajKale18 <vnkthebeast@gmail.com>
2024-03-21 12:34:33 +0530

    deployment changes

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -12,10 +12,10 @@ export const signupUser =async (req,res)=>{
         const hashedPassword = await bcrypt.hash(req.body.password,10);
         const user = { username: req.body.username, name: req.body.name, password: hashedPassword }
 
-      const newUser = new User(user);
+      const newUser = await new User(user);
       await newUser.save();
       
-      return res.status(200).json({msg:'SignUp Succesfull'})
+      return res.status(200).json({msg:'SignUp Succesfull',newUser})
     } catch (error) {
         return res.status(500).json({msg:'Error in Sign in'})
     }
